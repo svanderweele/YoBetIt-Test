e1977621c7754d28cc74e0f53b2ffcae6132116a
Simon Van Der Weele (Personal) <simonvanderweelepersonal@Simons-MacBook-Pro.local>
2020-04-28 04:53:24 +0200

    Created client react app with Typescript template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,24 @@
-import React from "react";
-import CountryList from "./components/CountryList/CountryList";
-import { Tabs, Tab } from "react-bootstrap";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Tabs defaultActiveKey="countries" id="uncontrolled-tab-example">
-        <Tab eventKey="countries" title="Countries">
-          <CountryList />
-        </Tab>
-        <Tab eventKey="slots" title="Slot Machine">
-        </Tab>
-      </Tabs>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
